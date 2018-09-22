983f8441f4d4dc56ace5f0a20278f39f20d190b8
renjuratheesh <renju.ratheesh@gmail.com>
2018-09-22 22:21:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Hello World & welcome to Jenkins Training *****-----");
+		System.out.println("-----***** Made Some Changes *****-----");
 	}
 }
